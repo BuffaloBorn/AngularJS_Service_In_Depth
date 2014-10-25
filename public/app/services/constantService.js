3f300aa3d101f49bbf58192377e38bc140a94768
Brice Wilson <brice@5wilsons.net>
2014-10-25 09:27:08 -0400

    added constant service

--- public/app/services/constantService.js
+++ public/app/services/constantService.js
@@ -1,3 +1,10 @@
-/**
- * Created by brice on 10/25/14.
- */
+(function () {
+
+    angular.module('app')
+        .constant('constants', {
+            APP_TITLE: 'Book Logger',
+            APP_DESCRIPTION: 'Track which books you read.',
+            APP_VERSION: '1.0'
+        });
+
+}());
